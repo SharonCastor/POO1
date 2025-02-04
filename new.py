5cb1a1c3419d3772e1ed5e53b854176107a85e6e
Sharon Castor <sharoncastor07@gmail.com>
2025-02-04 08:38:01 -0600

    update new.py

--- new.py
+++ new.py
@@ -1,1 +1,24 @@
-#Cuadrado
+#Figuras
+import math
+
+class Figuras:
+    def cal_perimetro(numLados, lado):
+        perimetro = numLados * lado
+        return perimetro
+
+    def cal_area(numLados, lado):
+        if numLados == 4:  
+            area = lado * lado
+        elif numLados == 3: 
+            area = (math.sqrt(3) / 4 ) * (lado ** 2 )
+        elif numLados == 5: 
+            area = (1/4) * math.sqrt(5 * (5 + 2 * math.sqrt(5))) * (lado ** 2)
+        else: area = 0
+        return area
+
+           
+        
+
+
+
+
