0ddcb8d39c6212f2b409e3c661966c6ebe019b4c
Sharon Castor <sharoncastor07@gmail.com>
2025-02-06 08:15:57 -0600

    update Arreglos.py

--- Arreglos.py
+++ Arreglos.py
@@ -4,7 +4,17 @@ class Estudiante:
         self.nombre = nombre
         self.edad = edad
 
-        def mostrar_info
+        def mostrar_info(self):
+            print(f"Nombre:{self.nombre}, Edad: {self.edad"})
+
+estudiantes = [
+    Estudiante = ("Alex", 19),
+    Estudiante = ("Sharon",18)
+    Estudiante = ("Fer",21)
+]
+
+for estudiante in estudiantes:
+    estudiante.mostrar_info()
 
 
 
