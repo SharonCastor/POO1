151652d6ea64af6d69a04ac73fd16789004c9e14
Sharon Castor <sharoncastor07@gmail.com>
2025-02-28 13:09:33 -0600

    update modificar.py

--- modificar.py
+++ modificar.py
@@ -1,11 +1,19 @@
-class Arreglos:
-    def __init__(self):
-        self.arregloList= (v)
-        def modificar(self,v):
-            index= int(input("lugar a cambiar"))
-            if 0 <= index < len(self.arregloList)
-            nuevoValor= int(input("Escribs el nuevo valor"))
-            self.arregloList[index]=nuevoValor
+#Práctica 4
 
-            esle:
-            print("Fuera de rango")
+class Arreglos:
+    def __init__(self, v):  # Se espera que 'v' sea una lista
+        self.arregloList = v
+
+    def modificar(self):
+        index = int(input("Lugar a cambiar: "))
+        if 0 <= index < len(self.arregloList):  # Se corrigió la sintaxis de 'if'
+            nuevoValor = int(input("Escribe el nuevo valor: "))
+            self.arregloList[index] = nuevoValor
+        else:
+            print("Fuera de rango")  # Se corrigió 'esle' a 'else'
+
+# Ejemplo de uso:
+mi_arreglo = Arreglos([1, 2, 3, 4, 5])
+mi_arreglo.modificar()
+print(mi_arreglo.arregloList)
+
