6d95b88612f27e68f6ea7537f8ed24d9bd84b2ef
Sharon Castor <sharoncastor07@gmail.com>
2025-05-19 19:36:38 -0600

    createPráctica 6.py

--- Flask(hola).py
+++ Flask(hola).py
@@ -14,3 +14,16 @@ def hello_world():
 
 if __name__ == '_main_':
     app.run(host='0.0.0.0', port=5000)
+
+
+from flask import Flask
+
+app = Flask (__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+if __name__ == '__main__':
+    ALLOWED_IP = '10.2.80.150'
+    app.run(host = '0.0.0.0', port = 5000)
